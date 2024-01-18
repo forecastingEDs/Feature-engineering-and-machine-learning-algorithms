e77d4d2fc3ed27a6d51d3bba2ec5a8f0cf9385cf
forecastingEDs <104029689+forecastingEDs@users.noreply.github.com>
2024-01-18 15:59:37 -0300

    Update Script_paper_1.R

--- Script_paper_1.R
+++ Script_paper_1.R
@@ -44,12 +44,17 @@ library(RcppParallel)
 library(FSelectorRcpp)
 library(catboost)
 
-# Parallel processing 
+# Parallel processing to speed up grid search 
 doParallel::registerDoParallel(cores=20)
 parallel_stop()
 # Divisão dos conjuntos de dados em treinamento e teste.
 
-# Cross validation com todos os preditores ----
+### Load Data ----
+### In this step you must import the data from the Excel spreadsheet to Rstudio keeping the original file name "atends_temperature_calendar" so that the next steps work correctly.
+With Rstudio open, in the upper right corner you have the option/tab >import Dataset>from excel>Browse and select the "datasets"
+
+
+# Splitting datasets into time series cross-validation training and testing ----
 
 data_tbl <- datasets %>%
   select(id, Date, attendences, average_temperature, min, max,  sunday, monday, tuesday, wednesday, thursday, friday, saturday, Jan, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec) %>%
@@ -58,8 +63,9 @@ data_tbl <- datasets %>%
 data_tbl
 
 
-##---##
-emergency_tscv <- data_tbl %>% filter(id == "aku") %>%
+# Selecting one ("arma") from the 11 datasets ----
+### Split the dataset into analyis/assessment set
+emergency_tscv <- data_tbl %>% filter(id == "arma") %>%
   time_series_cv (
     assess     = "7 days",
     skip        = "7 days",
@@ -71,8 +77,18 @@ emergency_tscv
 
 emergency_tscv  %>%  tk_time_series_cv_plan ()
 
+# View time series in training and testing split ----
+emergency_tscv %>%
+  tk_time_series_cv_plan() %>%
+  plot_time_series_cv_plan(date, value, .interactive = F ,
+                           .title = "arma")
+emergency_tscv %>%
+  tk_time_series_cv_plan() %>%
+  plot_time_series_cv_plan(date, value, 
+                           .facet_ncol = 2, .interactive = F)
 
-####  Feature selection step using a random forest ---- top_p = 16, threshold = 0.80 ----
+
+####  Feature selection step using a random forest ---- top_p = 10, or threshold = 0.70 ----
 
 recipe_spec <- recipe(value ~ ., 
                       data = training(emergency_tscv$splits[[1]])) %>%
@@ -82,31 +98,11 @@ recipe_spec <- recipe(value ~ .,
   step_select_forests(all_predictors(), scores = TRUE, threshold = 0.70, outcome = "value")
 
 recipe_spec %>% prep() %>% juice() %>% glimpse()
+summary(recipe_spec)
 
-
-## ver as variaveis de FE 
-
-recipe_spec <- recipe(value ~ ., 
-                      data = training(emergency_tscv$splits[[1]])) %>%
-  step_timeseries_signature(date) %>%
-  step_rm(matches("(.iso$)|(.xts$)|(.lbl$)|(hour)|(minute)|(second)|(am.pm)|(date_year$)|(date_day$)|(id$)|(date_month$)|(date_wday$)"))
-
-# Now a "DESIGN MATRIX" is made ----
-recipe_spec %>% prep() %>% juice() %>% glimpse()
-view(recipe_spec %>% prep() %>% juice() %>% glimpse()) 
-recipe_spec %>% prep() %>% juice()
-
-# A base é “1970-01-01 00:00:00” (execute "1970-01-01 00:00:00" 
-# %>% ymd_hms() %>% as.numeric()para ver se o valor retornado é zero).
-"1970-01-01 00:00:00" %>% ymd_hms() %>% as.numeric() 
-"1970-01-02 00:00:00" %>% ymd_hms() %>% as.numeric()
-"1970-01-03 00:00:00" %>% ymd_hms() %>% as.numeric()
-"2014-01-01 00:00:00" %>% ymd_hms() %>% as.numeric() 
-"2014-01-02 00:00:00" %>% ymd_hms() %>% as.numeric()
-"2014-01-03 00:00:00" %>% ymd_hms() %>% as.numeric()
-##
-
-# Model 1: grid search Xgboost ----
+### step 3
+### Training the six ML algorithms with grid-search
+# Model 1: grid-search Xgboost ----
 wflw_fit_xgboost <- workflow() %>%
   add_model(
     boost_tree("regression", min_n = tune(),
@@ -130,14 +126,12 @@ xgboost_best <-
 xgboost_best 
 
 
-
-
 metrics <- show_best(xgboost_tune_RF_7, metric = "rmse", n=5)
-export(metrics, "xgboost_tune_RF_7_westeinde.xlsx") 
+export(metrics, "xgboost_tune_RF_7_ARMA.xlsx") 
 
 
 metrics <- show_best(xgboost_tune_RF_45, metric = "rmse", n=5)
-export(metrics, "xgboost_tune_RF_45_westeinde.xlsx") 
+export(metrics, "xgboost_tune_RF_45_ARMA.xlsx") 
 
 
 # Model 1: Xgboost ----
@@ -181,26 +175,15 @@ SCORES <- wflw_fit_xgboost_best_RF_45 %>%
   tidy(number = 4, type = "scores")
 view(SCORES)
 
-
-
-export(SCORES, "scores_westeinde_XGBoost.xlsx") 
-
-
-
-scores_westeinde_XGBoost %>%
+SCORES %>%
   mutate(variable = fct_reorder(variable, score)) %>%
   ggplot( aes(x=variable, y=score)) +
   geom_bar(stat="identity", fill="black", alpha=.6, width=.4) +
   coord_flip() +
   xlab("") +
-  theme_bw() + labs(x = "Final predictor variables", y = "Importance scores (%)", title = "WESTEINDE - XGBoost")
+  theme_bw() + labs(x = "Final predictor variables", y = "Importance scores (%)", title = "ARMA - XGBoost")
 
-
-
-
-
-
-# Model 2: grid search LightGBM ----
+# Model 2: grid-search LightGBM ----
 wflw_fit_lightgbm <- workflow() %>%
   add_model(
     boost_tree("regression", min_n = tune(),
@@ -219,17 +202,16 @@ lightgbm_tune_RF_7 <- wflw_fit_lightgbm
 show_best(lightgbm_tune_RF_7, metric = "rmse", n=5)
 
 
-
 lightgbm_tune_RF_45 <- wflw_fit_lightgbm
 show_best(lightgbm_tune_RF_45, metric = "rmse", n=5)
 
 
 metrics <- show_best(lightgbm_tune_RF_7, metric = "rmse", n=5)
-export(metrics, "lightgbm_tune_7_WESTEINDE.xlsx") 
+export(metrics, "lightgbm_tune_7_ARMA.xlsx") 
 
 
 metrics <- show_best(lightgbm_tune_RF_45, metric = "rmse", n=5)
-export(metrics, "lightgbm_tune_45_WESTEINDE.xlsx") 
+export(metrics, "lightgbm_tune_45_ARMA.xlsx") 
 
 lightgbm_best <- 
   wflw_fit_lightgbm %>% 
@@ -275,19 +257,16 @@ SCORES <- wflw_fit_lightgbm_best_RF_45 %>%
   tidy(number = 4, type = "scores")
 view(SCORES)
 
-export(SCORES, "scores_WESTEINDE_LightGBM.xlsx") 
-
-
-scores_WESTEINDE_LightGBM %>%
+SCORES %>%
   mutate(variable = fct_reorder(variable, score)) %>%
   ggplot( aes(x=variable, y=score)) +
   geom_bar(stat="identity", fill="black", alpha=.6, width=.4) +
   coord_flip() +
   xlab("") +
-  theme_bw() + labs(x = "Final predictor variables", y = "Importance scores (%)", title = "WESTEINDE - LightGBM")
+  theme_bw() + labs(x = "Final predictor variables", y = "Importance scores (%)", title = "ARMA - LightGBM")
 
 
-# Model 3: grid search Random Forest by ranger ---- 
+# Model 3: grid-search Random Forest by ranger ---- 
 wflw_fit_rf <- workflow() %>%
   add_model(
     rand_forest("regression", mtry = tune(),  trees = tune(), min_n = tune()) %>% set_engine("ranger", num.threads = 20)
@@ -312,11 +291,11 @@ RF_best
 
 
 metrics <- show_best(RF_tune_RF_7, metric = "rmse", n=5)
-export(metrics, "RF_tune_7_WESTEINDE.xlsx") 
+export(metrics, "RF_tune_7_ARMA.xlsx") 
 
 
 metrics <- show_best(RF_tune_RF_45, metric = "rmse", n=5)
-export(metrics, "RF_tune_RF_45_WESTEINDE.xlsx") 
+export(metrics, "RF_tune_RF_45_ARMA.xlsx") 
 
 
 
@@ -351,19 +330,17 @@ SCORES <- wflw_fit_rf_best_RF_45 %>%
   tidy(number = 4, type = "scores")
 view(SCORES)
 
-export(SCORES, "scores_WESTEINDE_RF.xlsx") 
-
-scores_WESTEINDE_RF %>%
+SCORES %>%
   mutate(variable = fct_reorder(variable, score)) %>%
   ggplot( aes(x=variable, y=score)) +
   geom_bar(stat="identity", fill="black", alpha=.6, width=.4) +
   coord_flip() +
   xlab("") +
-  theme_bw() + labs(x = "Final predictor variables", y = "Importance scores (%)", title = "WESTEINDE - RF")
+  theme_bw() + labs(x = "Final predictor variables", y = "Importance scores (%)", title = "ARMA - RF")
 
 
 # Radial Basis Function Support Vector Machine
-# Model 4: SVM-RBF grid search  -----
+# Model 4: SVM-RBF grid-search  -----
 wflw_svm_rbf <- workflow() %>%
   add_model(
     svm_rbf ("regression", cost = tune(), rbf_sigma = tune(), margin =tune()) %>% set_engine("kernlab", num.threads = 20))%>%
@@ -385,11 +362,11 @@ svm_rbf_best
 
 
 metrics <- show_best(svm_rbf_tune_RF_7, metric = "rmse", n=5)
-export(metrics, "svm_rbf_tune_RF_7_WESTEINDE.xlsx") 
+export(metrics, "svm_rbf_tune_RF_7_ARMA.xlsx") 
 
 
 metrics <- show_best(svm_rbf_tune_RF_45, metric = "rmse", n=5)
-export(metrics, "svm_rbf_tune_RF_45_WESTEINDE.xlsx") 
+export(metrics, "svm_rbf_tune_RF_45_ARMA.xlsx") 
 
 
 # Model 4: SVM-RBF -----
@@ -426,21 +403,16 @@ SCORES <- wflw_fit_svm_rbf_RF_45 %>%
 view(SCORES)
 
 
-
-export(SCORES, "scores_WESTEINDE_SVM.xlsx") 
-
-
-
-scores_WESTEINDE_SVM %>%
+SCORES %>%
   mutate(variable = fct_reorder(variable, score)) %>%
   ggplot( aes(x=variable, y=score)) +
   geom_bar(stat="identity", fill="black", alpha=.6, width=.4) +
   coord_flip() +
   xlab("") +
-  theme_bw() + labs(x = "Final predictor variables", y = "Importance scores (%)", title = "WESTEINDE - SVM-RBF")
+  theme_bw() + labs(x = "Final predictor variables", y = "Importance scores (%)", title = "ARMA - SVM-RBF")
 
 # Model 5: NNAR grid-search -----
-# Model 5: grid search NNAR MaxNWts = 5731, 7001, 8000, 401 ----- MaxNWts = 401
+# Model 5: grid-search ----
 wflw_fit_NNAR <- workflow() %>% 
   add_model(
     nnetar_reg(penalty = tune(), hidden_units =tune(), num_networks=tune()) %>%
@@ -464,10 +436,10 @@ NNAR_best
 
 
 metrics <- show_best(NNAR_tune_RF_7, metric = "rmse", n=5)
-export(metrics, "NNAR_tune_RF_7_WESTEINDE.xlsx") 
+export(metrics, "NNAR_tune_RF_7_ARMA.xlsx") 
 
 metrics <- show_best(NNAR_tune_RF_45, metric = "rmse", n=5)
-export(metrics, "NNAR_tune_RF_45_WESTEINDE.xlsx") 
+export(metrics, "NNAR_tune_RF_45_ARMA.xlsx") 
 
 
 # Model 5: NNAR -----
@@ -497,21 +469,16 @@ SCORES <- wflw_fit_ANN_best_RF_45 %>%
 view(SCORES)
 
 
-
-export(SCORES, "scores_WESTEINDE_NNAR.xlsx") 
-
-
-
-scores_WESTEINDE_NNAR %>%
+SCORES %>%
   mutate(variable = fct_reorder(variable, score)) %>%
   ggplot( aes(x=variable, y=score)) +
   geom_bar(stat="identity", fill="black", alpha=.6, width=.4) +
   coord_flip() +
   xlab("") +
-  theme_bw() + labs(x = "Final predictor variables", y = "Importance scores (%)", title = "WESTEINDE - NNAR")
+  theme_bw() + labs(x = "Final predictor variables", y = "Importance scores (%)", title = "ARMA - NNAR")
 
 
-# Model 6: GLMNET grid search  ----
+# Model 6: GLMNET grid-search  ----
 wflw_fit_glmnet <- workflow() %>%
   add_model(
     linear_reg(penalty = tune(),mixture = tune()
@@ -536,11 +503,11 @@ glmnet_best
 
 
 metrics <- show_best(glmnet_tune_RF_7 , metric = "rmse", n=5)
-export(metrics, "glmnet_tune_RF_7_WESTEINDE.xlsx") 
+export(metrics, "glmnet_tune_RF_7_ARMA.xlsx") 
 
 
 metrics <- show_best(glmnet_tune_RF_45, metric = "rmse", n=5)
-export(metrics, "glmnet_tune_RF_45_WESTEINDE.xlsx") 
+export(metrics, "glmnet_tune_RF_45_ARMA.xlsx") 
 
 
 # Model 6: GLMNET  ----
@@ -571,22 +538,22 @@ SCORES <- wflw_fit_glmnet_best_RF_45 %>%
   tidy(number = 4, type = "scores")
 view(SCORES)
 
-export(SCORES, "scores_WESTEINDE_GLMNET.xlsx") 
-
-
-
-scores_WESTEINDE_GLMNET %>%
+SCORES %>%
   mutate(variable = fct_reorder(variable, score)) %>%
   ggplot( aes(x=variable, y=score)) +
   geom_bar(stat="identity", fill="black", alpha=.6, width=.4) +
   coord_flip() +
   xlab("") +
-  theme_bw() + labs(x = "Final predictor variables", y = "Importance scores (%)", title = "WESTEINDE - GLMNET")
+  theme_bw() + labs(x = "Final predictor variables", y = "Importance scores (%)", title = "ARMA - GLMNET")
 
 
 
 
-#modeltable ----
+### step 4
+### Create Modeltime Table ----
+### The Modeltime Table organizes the models with IDs and creates generic descriptions to help us keep track of our models. 
+### Let's add the models to a modeltime_table()
+### table modeltime ----
 
 model_tbl <- modeltime_table(wflw_fit_xgboost_best_RF_7,
                              wflw_fit_lightgbm_best_RF_7,
@@ -610,7 +577,7 @@ model_tbl
 
 
 
-#Previsão de cross-validation -----
+# 5-fold cross-validation prediction of time series ----
 resample_results <- model_tbl %>%
   modeltime_fit_resamples(
     resamples = emergency_tscv,
@@ -620,31 +587,27 @@ resample_results <- model_tbl %>%
 
 resample_results
 
-
-
-##---## resample_results -----
+##---## Results by 5-fold resampling -----
 resample_results %>%
   modeltime_resample_accuracy(summary_fns = NULL, yardstick::metric_set(mape, smape, mase, rmse)) %>%
   table_modeltime_accuracy(.interactive = FALSE)
 
 resample_results_fitted 
 
-##---## resample_results -----
+##---## Average results of five folds of resampling -----
 resample_results %>%
   modeltime_resample_accuracy(summary_fns = mean, yardstick::metric_set(mape, smape, mase, rmse)) %>%
   table_modeltime_accuracy(.interactive = FALSE)
 
 
-emf("bar.emf")
-
-##---##
+## Charts to compare the performance of the models across the four evaluation metrics (MAPE, sMAPE, MASE, and RMSE)----
 resample_results %>%
   plot_modeltime_resamples(yardstick::metric_set(mape, smape, mase, rmse),
                            .point_size  = 4,
                            .summary_line_size  =  1,
                            .point_alpha = 0.8,
                            .interactive = FALSE,
-                           .title =  "WESTEINDE - 7-day test set",
+                           .title =  "ARMA - 7-day test set",
                            .color_lab =  "Algorithms"
   )
 
@@ -654,8 +617,6 @@ resample_results %>%
                            .summary_line_size  =  1,
                            .point_alpha = 0.8,
                            .interactive = FALSE,
-                           .title =  "WESTEINDE - 45-day test set",
+                           .title =  "ARMA - 45-day test set",
                            .color_lab =  "Algorithms"
   )
-
-
